88c3a9d6ce1b9ed46c56052fd8439e469f805b41
Hanas <hanasoke@gmail.com>
2025-11-26 21:21:36 +0700

    the last update of laptop_inventory

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def create_laptop():
     code = input("Laptop Code: ")
     name = input("Laptop Name: ")
     stock = int(input("Stock: "))
-    price = float(input("Price"))
+    price = float(input("Price: "))
     
     laptop = {
         "code": code,
@@ -26,8 +26,62 @@ def read_laptops():
         return 
     
     for i, laptop in enumerate(laptops, start=1):
-        print(f"{i}. Code: {laptop['code']} | Name: {laptop['name']} | {laptop['stock']} | Price: {laptop['harga']} ")
+        print(f"{i}. Code: {laptop['code']} | Name: {laptop['name']} | {laptop['stock']} | Price: {laptop['price']} ")
     print()
     
 def update_laptop(): 
     print("\n=== DELETE LAPTOP DATA ===")
+    read_laptops()
+    code = input("Input the updated laptop code: ")
+    
+    for laptop in laptops: 
+        if laptop['code'] == code:
+            print("Data has been found! Please Update.")
+            laptop['name'] = input("New Name: ")
+            laptop['stock'] = int(input("New Stock: "))
+            laptop['price'] = float(input("New Price: "))
+            print("New Data has been updated!\n")
+            return 
+
+    print("No New Data found!\n")
+    
+def delete_laptop():
+    print("\n=== DELETE LAPTOP DATA ===")
+    read_laptops()
+    code = input("Enter the laptop code that will be removed: ")
+    
+    for laptop in laptops:
+        if laptop['code'] == code: 
+            laptops.remove(laptop)
+            print("Data has been deleted!\n")
+            return 
+        
+    print("No data found.\n")
+    
+def main_menu(): 
+    while True: 
+        print("=== LAPTOP INVENTORY MENU ===")
+        print("1. Add New Laptop")
+        print("2. Look The Data")
+        print("3. Update Data")
+        print("4. Delete Data")
+        print("5. Exit")
+        
+        choice = input("Choice A Menu: ")
+        
+        if choice == '1':
+            create_laptop()
+        elif choice == '2':
+            read_laptops()
+        elif choice == '3':
+            update_laptop()
+        elif choice == '4':
+            delete_laptop()
+        elif choice == '5':
+            print("Exit from Program...")
+            break 
+        else: 
+            print("No Valid Choice!\n")
+        
+if __name__ == '__main__':
+    main_menu()
